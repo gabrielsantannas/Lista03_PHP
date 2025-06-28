1f6aebcd881d8ff77e19bb964141e1eb28adaa30
Gabriel SantAnna <gsantanna00517@gmail.com>
2025-06-28 12:24:21 -0300

    Atividade15

---
+++ Atividade15/atividade15.php
@@ -0,0 +1,15 @@
+<?php
+
+$vrvenda = $_POST ['vrvenda'];
+$comissao;
+
+if ($vrvenda <= 1000) {
+    $comissao =  ($vrvenda * 5 /100);
+} elseif ($comissao <= 5000) {
+    $comissao = ($vrvenda * 10 /100);
+} else {
+    $comissao = ($vrvenda * 15 /100);
+}
+
+echo "O valor da sua comissão é: R$" . number_format($comissao, 2, ',','.'); 
+?>
