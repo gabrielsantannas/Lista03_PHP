fc559969fd98575aec2b0001977dc5979f6b5e9b
Gabriel SantAnna <gsantanna00517@gmail.com>
2025-06-28 09:31:24 -0300

    Atividade03

---
+++ Atividade03/atividade03.php
@@ -0,0 +1,16 @@
+<?php
+
+$nota1 = $_POST ['nota1'];
+$nota2 = $_POST ['nota2'];
+$nota3 = $_POST ['nota3'];
+
+$media = ($nota1 + $nota2 + $nota3) / 3;
+echo "Sua média é igual a: " . number_format($media, 1, ',',);;
+
+if ($media >= 7) {
+    echo "<br>Você está aprovado";
+} elseif ($media >= 5 && $media < 7) {
+    echo "<br>Você está de recuperação";
+} else {
+    echo "<br>Você está reprovado";
+}
